894c73b5ddf8350859e5614b23ec768e78642162
Christian Kubicka <kubickce@stanford.edu>
2025-10-22 13:28:42 -0700

    Configured load_modules.sh to work with tcsh.  Added dc_shell to the module list.

--- load_modules.sh
+++ load_modules.sh
@@ -1,6 +1,8 @@
+module purge
 module load base
 module load vcs/S-2021.09-SP1
+module load dc_shell/S-2021.06-SP5-4
 
-# Getting the Script Path
-SCRIPT_PATH=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
-export PATH=$SCRIPT_PATH/simulator/bin:$PATH
+set SCRIPT_NAME = `lsof +p $$ | \grep -oE /.\*load_modules.sh`
+set SCRIPT_PATH = `dirname $filename`
+setenv PATH $SCRIPT_PATH/simulator/bin:$PATH
