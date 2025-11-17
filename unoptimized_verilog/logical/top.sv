f87410a34bad5fd3eb4cc235f87da35cd29024e9
Yujie Shern <shern@email.virginia.edu>
2025-11-17 00:03:59 -0800

    done top

--- unoptimized_verilog/logical/top.sv
+++ unoptimized_verilog/logical/top.sv
@@ -27,6 +27,47 @@ module top(
     // START IMPLEMENTATION
     // Your Code Here
     // END IMPLEMENTATION
+    pe_inst_t  pe_inst;
+    logic      pe_inst_valid;
+    buf_inst_t buf_inst;
+    logic      buf_inst_valid;
+    controller u_controller(
+        .clk(clk), 
+        .rst_n(rst_n),
+        .inst(inst),
+        .inst_valid(inst_valid),
+        .inst_exec_begins(inst_exec_begins),
+        .pe_inst(pe_inst),
+        .pe_inst_valid(pe_inst_valid),
+        .buf_inst(buf_inst),
+        .buf_inst_valid(buf_inst_valid)
+    );
 
+    logic [`MEM0_BITWIDTH-1:0] matrix_data;
+    logic [`MEM1_BITWIDTH-1:0] vector_data;
+    logic [`MEM2_BITWIDTH-1:0] output_data;
+    buffer u_buffer(
+        .clk(clk),
+        .rst_n(rst_n),
+        .buf_inst(buf_inst),
+        .buf_inst_valid(buf_inst_valid),
+        .matrix_data(matrix_data),
+        .vector_data(vector_data),
+        .output_data(output_data)
+    );
 
+    logic [`PE_OUTPUT_BITWIDTH-1:0] pe_outputs [`PE_COUNT-1:0];
+    genvar i;
+    generate
+        for (i=0; i< `PE_COUNT; i=i+1) begin : PE_ARRAY
+            .clk(clk),
+            .rst_n(rst_n),
+            .pe_inst(pe_inst),
+            .pe_inst_valid(pe_inst_valid),
+            .vector_input(vector_data),
+            .matrix_input(matrix_data[i*`PE_INPUT_BITWIDTH-1 +: `PE_INPUT_BITWIDTH]),
+            .vector_output(pe_outputs[i])
+        end 
+        assign output_data[i*`PE_OUTPUT_BITWIDTH-1 +: `PE_OUTPUT_BITWIDTH] = pe_outputs[i];
+    endgenerate
 endmodule
